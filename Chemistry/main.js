df65dc8eb0cf835d57e11430b06bff6442cd4f14
zbot473 <zbot473@gmail.com>
2018-11-20 00:37:46 -0500

    Electron configuration superscript

--- Chemistry/main.js
+++ Chemistry/main.js
@@ -1,6 +1,6 @@
 var req = new XMLHttpRequest();
 var elements;
-req.onload = function() {
+req.onload = function () {
     elements = JSON.parse(this.responseText)
 };
 req.open('GET', 'https://raw.githubusercontent.com/zbot473/zbot473.github.io/master/Chemistry/properties.json');
@@ -8,75 +8,92 @@ req.send();
 
 function showProperty(html) {
     document.getElementById("propertytable").removeAttribute("hidden")
-    var elementJson = elements.filter(function(elements) {
+    var elementJson = elements.filter(function (elements) {
         return elements.symbol == html.innerHTML
     })["0"]
-    var name = document.getElementById("name");
-    var anumber = document.getElementById("anumber");
-    var econfig = document.getElementById("econfig");
-    var ostate = document.getElementById("ostate");
-    var aradius = document.getElementById("aradius");
-    var eneg = document.getElementById("eneg");
-    var state = document.getElementById("state");
-    var mpoint = document.getElementById("mpoint");
-    var bpoint = document.getElementById("bpoint");
-    var amass = document.getElementById("amass");
-    var ienergy = document.getElementById("ienergy");
-    var density = document.getElementById("density");
+    var name = document.getElementById("name"),
+        anumber = document.getElementById("anumber"),
+        econfig = document.getElementById("econfig"),
+        ostate = document.getElementById("ostate"),
+        aradius = document.getElementById("aradius"),
+        eneg = document.getElementById("eneg"),
+        state = document.getElementById("state"),
+        mpoint = document.getElementById("mpoint"),
+        bpoint = document.getElementById("bpoint"),
+        amass = document.getElementById("amass"),
+        ienergy = document.getElementById("ienergy"),
+        density = document.getElementById("density"),
+        btype = document.getElementById("btype"),
+        ydiscover = document.getElementById("ydiscover"),
+        electronicConfiguration = elementJson.electronicConfiguration.split(/((?<=\w)(\d))/)
+
     name.innerHTML = elementJson.name;
     anumber.innerHTML = elementJson.atomicNumber;
-    econfig.innerHTML = elementJson.electronicConfiguration;
-    sublevels = elementJson.electronicConfiguration
-    console.log(sublevels)
-    amass.innerHTML = elementJson.atomicMass + " amu";
-    if (elementJson.oxidationStates){
-        ostate.innerHTML = elementJson.oxidationStates;
+    for (let i = 0; i < electronicConfiguration.length; i++) {
+        const e = electronicConfiguration[i];
+        if (e.match(/\b\d\b/g) != null) {
+            if (electronicConfiguration[i + 1].match(/\b\d\b/g) != null) {
+                electronicConfiguration.splice(i, 1)
+            }
+        }
     }
-    else { 
+    for (let i = 0; i < electronicConfiguration.length; i++) {
+        const e = electronicConfiguration[i];
+        if (e.match(/\b\d\b/g) != null) {
+            electronicConfiguration[i]="<sup>"+electronicConfiguration[i]+"</sup>"
+        }
+    }
+    console.log(electronicConfiguration)
+    econfig.innerHTML = electronicConfiguration.join("")
+    amass.innerHTML = elementJson.atomicMass + " amu";
+    if (elementJson.oxidationStates) {
+        ostate.innerHTML = elementJson.oxidationStates;
+    } else {
         ostate.innerHTML = "Unknown"
     }
-    if (elementJson.vanDelWaalsRadius){
+    if (elementJson.vanDelWaalsRadius) {
         aradius.innerHTML = elementJson.vanDelWaalsRadius + " pm";
-    }
-    else { 
+    } else {
         aradius.innerHTML = "Unknown"
     }
-    if (elementJson.electronegativity){
+    if (elementJson.electronegativity) {
         eneg.innerHTML = elementJson.electronegativity;
-    }
-    else { 
+    } else {
         eneg.innerHTML = "Unknown"
     }
-    if (elementJson.standardState){
+    if (elementJson.standardState) {
         state.innerHTML = elementJson.standardState;
-    }
-    else { 
+    } else {
         state.innerHTML = "Unknown"
     }
-    if (elementJson.density){
-        density.innerHTML = elementJson.density + "g/cm" + "3".sup();
-    }
-    else { 
+    if (elementJson.density) {
+        density.innerHTML = elementJson.density + "g/cm" + "<sup>3</sup>";
+    } else {
         density.innerHTML = "Unknown"
     }
-    if (elementJson.ionizationEnergy){
+    if (elementJson.ionizationEnergy) {
         ienergy.innerHTML = elementJson.ionizationEnergy + " kJ/mol";
-    }
-    else { 
+    } else {
         ienergy.innerHTML = "Unknown"
     }
-    if (elementJson.meltingPoint){
+    if (elementJson.meltingPoint) {
         mpoint.innerHTML = elementJson.meltingPoint + "˚C";
-    }
-    else { 
+    } else {
         mpoint.innerHTML = "Unknown"
     }
-    if (elementJson.boilingPoint){
+    if (elementJson.boilingPoint) {
         bpoint.innerHTML = elementJson.boilingPoint + "˚C";
-    }
-    else { 
+    } else {
         bpoint.innerHTML = "Unknown"
     }
+    if (elementJson.meltingPoint) {
+        btype.innerHTML = elementJson.bondingType
+    } else {
+        btype.innerHTML = "Unknown"
+    }
+    
+    ydiscover.innerHTML = elementJson.yearDiscovered
+
 }
 
 function hideProperty() {
